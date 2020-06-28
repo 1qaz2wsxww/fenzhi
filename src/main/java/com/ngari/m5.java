836d9d3c784c8ef9c1183c7915dad28bfb929c57
wuheng <heng.wu@goupwith.com>
2020-06-28 19:16:13 +0800

    m5

---
+++ src/main/java/com/ngari/m5.java
@@ -0,0 +1,14 @@
+package com.ngari;
+
+/**
+ * @program: fenzhi
+ * @description:
+ * @author: WuHeng
+ * @create: 2020-02-02 22:31
+ **/
+
+public class m5 {
+    public static void main(String[] args) {
+        System.out.println("m5");
+    }
+}
