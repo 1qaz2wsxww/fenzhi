96943ed43d2c82d8afb1ae9a8b8b69bffbb44087
wuheng <wuheng@winning.com>
2020-12-17 09:33:49 +0800

    new m6  m7

---
+++ src/main/java/com/ngari/m7.java
@@ -0,0 +1,15 @@
+package com.ngari;
+
+/**
+ * @program: fenzhi
+ * @description:
+ * @author: WuHeng
+ * @create: 2020-02-02 22:31
+ **/
+
+public class m7 {
+    public static void main(String[] args) {
+        System.out.println("m77777777777");
+
+    }
+}
