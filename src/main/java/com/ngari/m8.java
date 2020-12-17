18159244cf1e306822fc99ed6ee76e090fc58a69
wuheng <wuheng@winning.com>
2020-12-17 09:36:06 +0800

    new m8

---
+++ src/main/java/com/ngari/m8.java
@@ -0,0 +1,15 @@
+package com.ngari;
+
+/**
+ * @program: fenzhi
+ * @description:
+ * @author: WuHeng
+ * @create: 2020-02-02 22:31
+ **/
+
+public class m8 {
+    public static void main(String[] args) {
+        System.out.println("m8888888888");
+
+    }
+}
