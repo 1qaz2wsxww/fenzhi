f20fedd4c760f161bc8071f682cfa1ed82200e09
wuheng <wuheng@winning.com>
2020-12-17 10:02:19 +0800

    new m12

---
+++ src/main/java/com/ngari/m12.java
@@ -0,0 +1,14 @@
+package com.ngari;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: WuHeng
+ * @create: 2020-12-17 09:44
+ **/
+
+public class m12 {
+    public static void main(String[] args) {
+        System.out.println("m10");
+    }
+}
