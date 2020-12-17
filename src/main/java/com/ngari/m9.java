fc024e3c0d55773e33972590ae06c3e45b8dea13
wuheng <wuheng@winning.com>
2020-12-17 09:45:25 +0800

    new m9

---
+++ src/main/java/com/ngari/m9.java
@@ -0,0 +1,16 @@
+package com.ngari;
+
+import java.sql.SQLOutput;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: WuHeng
+ * @create: 2020-12-17 09:44
+ **/
+
+public class m9 {
+    public static void main(String[] args) {
+        System.out.println("m9");
+    }
+}
